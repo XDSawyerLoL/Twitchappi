8f78df169b2c13a90402b488fb32134ac8d204b5
XDSawyerLoL <74513223+XDSawyerLoL@users.noreply.github.com>
2026-01-12 22:22:39 +0100

    Delete public directory

--- public/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-// StreamerHub - ES Module entrypoint (single HTML page)
-import "./core/app-bootstrap.js";
-import "./modules/player/ambilight-vibe.js";
-import "./modules/layout/sidepanel-dock-fix.js";
-import "./modules/market/market-overlay.js";
-import "./modules/ui/sidepanel-tabs.js";
-import "./modules/ui/help-tooltips.js";
-
-// Note: modules above attach required handlers to window for inline onclick="..."
