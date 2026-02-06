4faeeef7cfa9529ebc91fee5f99e42d0f6a31dd1
XDSawyerLoL <74513223+XDSawyerLoL@users.noreply.github.com>
2026-02-06 16:14:16 +0100

    Add files via upload

--- assets/js/core/app-bootstrap.js
+++ assets/js/core/app-bootstrap.js
@@ -1118,14 +1118,7 @@ try{
       renderTwitFlix();
     }
 
-    
-// Alias for branding: THMAX is the new name of the TwitFlix modal
-window.openTHMAX = function(){
-  try { openTwitFlix(); } catch(e){ console.error('[THMAX] open failed', e); }
-};
-
-
-function closeTwitFlix(){
+    function closeTwitFlix(){
   document.body.classList.remove('modal-open');
   tfModalOpen = false;
   document.querySelectorAll('.tf-card.previewing').forEach(tfStopPreview);
